8439a8fc4d95729045984fe9ef56a04ae8d1e7b0
whichxjy <whichxjy@gmail.com>
2019-11-28 05:29:02 +0800

    Flush the output buffer

--- bounce-wm.c
+++ bounce-wm.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 #include <X11/Xlib.h>
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+// stop moving?
+volatile sig_atomic_t stop;
+
+// handle SIGINT signal
+void inthand(int signum) {
+    stop = 1;
+}
 
 int main() {
     // open connection with the display server
@@ -54,11 +64,14 @@ int main() {
     int win_height = 0;
 
     // the speed of the subwindow
-    int x_speed = 10;
-    int y_speed = 10;
+    int x_speed = 5;
+    int y_speed = 5;
 
     // keyboard event
     XEvent ev;
+
+    // catch the SIGINT signal
+    signal(SIGINT, inthand);
 
     while (1) {
         // pick next event
@@ -68,7 +81,7 @@ int main() {
             continue;
         }
 
-        if (ev.xkey.subwindow != None) {
+        if (ev.xkey.keycode == e_keycode && ev.xkey.subwindow != None) {
             // set current subwindow
             sub_win = ev.xkey.subwindow;
 
@@ -83,31 +96,42 @@ int main() {
             win_width = attr.width;
             win_height = attr.height;
         }
-
-        if (ev.xkey.keycode == e_keycode && sub_win != None) {
-            // next position
-            x_pos = MAX(MIN(x_pos + x_speed, display_width - win_width), 0);
-            y_pos = MAX(MIN(y_pos + y_speed, display_height - win_height), 0);
-
-            // collision detection
-            if (x_pos == 0 || (x_pos + win_width) == display_width) {
-                x_speed = -x_speed;
-            }
-            if (y_pos == 0 || (y_pos + win_height) == display_height) {
-                y_speed = -y_speed;
-            }
-
-            // move subwindow
-            XMoveWindow(dpy, sub_win, x_pos, y_pos);
-        }
         else if (ev.xkey.keycode == q_keycode) {
             // quit
             break;
+        }
+        
+        if (sub_win != None) {
+            // stop moving?
+            stop = 0;
+
+            while (!stop) {
+                // next position
+                x_pos = MAX(MIN(x_pos + x_speed, display_width - win_width), 0);
+                y_pos = MAX(MIN(y_pos + y_speed, display_height - win_height), 0);
+
+                // collision detection
+                if (x_pos == 0 || (x_pos + win_width) == display_width) {
+                    x_speed = -x_speed;
+                }
+                if (y_pos == 0 || (y_pos + win_height) == display_height) {
+                    y_speed = -y_speed;
+                }
+
+                // move subwindow
+                XMoveWindow(dpy, sub_win, x_pos, y_pos);
+
+                // flush the output buffer
+                XSync(dpy, False);
+
+                // sleep for 90 ms
+                usleep(90000);
+            }
         }
     }
 
     // close connection to display server
     XCloseDisplay(dpy);
 
-    return EXIT_SUCCESS;
+    exit(EXIT_SUCCESS);
 }
